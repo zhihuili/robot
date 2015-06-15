bce94e0a6e4380c2b7714df47b32ae7d5df992c3
dqq <dqq@dqqdeMacBook-Pro.local>
2015-06-15 15:38:26 +0800

    add notes

--- src/main/java/com/nana/robot/ui/AIRobot.java
+++ src/main/java/com/nana/robot/ui/AIRobot.java
@@ -3,11 +3,12 @@ package com.nana.robot.ui;
 import com.nana.common.mq.MqFactory;
 
 public class AIRobot {
-
+	//static method
 	public static void main(String[] args) {
 		startRobot();
 	}
-
+	
+	//start a robot
 	public static void startRobot() {
 		MqFactory.startMqConsumer("CID_DEV_NANA_1", "DEV_NANA_1",
 				RobotConsumerListener.getInstance());
