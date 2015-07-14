a6527340a77fc5406468cf6ac7ef4774cd66924e
wds8237 <823708266@qq.com>
2015-07-14 08:56:03 +0800

    recommit

--- src/main/java/com/nana/serviceengine/domain/commonapi/htmlcenter/Test.java
+++ src/main/java/com/nana/serviceengine/domain/commonapi/htmlcenter/Test.java
@@ -9,21 +9,21 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		TestBean bean = new TestBean();
-//		String res = HtmlCenter.getInstance().getHtml("frm.vm", bean);
-//		System.out.println(res);
-		
-		List<Map<String,String>> inputs=new ArrayList<Map<String,String>>();
-		HashMap<String, String> input1 = new HashMap<String, String>();
-        input1.put("id", "title");
-        input1.put("title", "标题：");
-        inputs.add(input1);
-        HashMap<String, String> input2 = new HashMap<String, String>();
-        input2.put("id", "brief");
-        input2.put("title", "摘要：");
-        inputs.add(input2);
-		String res = HtmlCenter.getInstance().getHtmlByList("frm.vm", inputs, "inputs");
+		TestBean bean = new TestBean();
+		String res = HtmlCenter.getInstance().getHtmlByBean("frm.vm", bean);
 		System.out.println(res);
+		
+//		List<Map<String,String>> inputs=new ArrayList<Map<String,String>>();
+//		HashMap<String, String> input1 = new HashMap<String, String>();
+//        input1.put("id", "title");
+//        input1.put("title", "标题：");
+//        inputs.add(input1);
+//        HashMap<String, String> input2 = new HashMap<String, String>();
+//        input2.put("id", "brief");
+//        input2.put("title", "摘要：");
+//        inputs.add(input2);
+//		String res = HtmlCenter.getInstance().getHtmlByList("frm.vm", inputs, "inputs");
+//		System.out.println(res);
 	}
 
 }
