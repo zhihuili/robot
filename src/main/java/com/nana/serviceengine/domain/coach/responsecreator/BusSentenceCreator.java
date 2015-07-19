1a4e12833e11368d332c55e7a1161d2a548bb760
yianemail <yianemail@sina.com>
2015-07-19 15:03:41 +0800

    bus is ok

--- src/main/java/com/nana/serviceengine/domain/coach/responsecreator/BusSentenceCreator.java
+++ src/main/java/com/nana/serviceengine/domain/coach/responsecreator/BusSentenceCreator.java
@@ -18,9 +18,7 @@ import com.nana.serviceengine.neuron.responsecreator.SentenceCreator;
 public class BusSentenceCreator implements SentenceCreator {
 
 	private static BusSentenceCreator wsc = new BusSentenceCreator();
-	private String mStr = "mStr";
 	Map<String, List<Bus>> map = new HashMap<String, List<Bus>>();
-	private int i = 1;
 
 	private BusSentenceCreator() {
 
@@ -36,22 +34,35 @@ public class BusSentenceCreator implements SentenceCreator {
 		Map<String, ParamItem> paramItems = params.getParams();
 		ResponseDisplay responseDisplay = new ResponseDisplay();
 		Integer index = (Integer) paramItems.get("indexChange").getValue();
-		int choice = (Integer) paramItems.get("choice").getValue();
+		Integer choice = (Integer) paramItems.get("choice").getValue();
+		if(index==null){
+			index=1;	
+		}
 		List<Bus> bus = params.getResult();
+		System.out.println("choice"+choice);
+		if(bus==null||bus.size()==0){
+			rma.setAudioText("对不起，没有数据。");
+			return rma;
+		}
 		if (bus.size() <= 5) {
-			if (index != 1) {
+			if (index!=null&&index != 1) {
 				rma.setAudioText("对不起，已经没有数据。");
 				return rma;
 			}
-			if (choice != -1 && choice < bus.size()) {
+			if (choice!=null&&choice != -1 && choice <=bus.size()) {
+				List <Bus> busDingdan=new ArrayList<Bus>();
+				busDingdan.add(bus.get(choice-1));
 				String choiceContent1 = HtmlCenter.getInstance().getHtmlByList(
-						"bus.vm", bus.get(choice), "inputs", "videohtml");
+						"bus.vm", busDingdan, "inputs", "demandhtml");
 				responseDisplay.setDataType("1");
 				responseDisplay.setHeight("0.8");
 				responseDisplay.setContent(choiceContent1);
 				rma.setDisplayText(JSON.toJSONString(responseDisplay));
 				return rma;
-			} else if (choice != -1 && choice > bus.size()) {
+				
+				
+				
+			} else if (choice!=null&&choice != -1 && choice > bus.size()) {
 				rma.setAudioText("对不起，此列表没有您选择的班次信息，请重新选择");
 				return rma;
 			}
@@ -68,13 +79,15 @@ public class BusSentenceCreator implements SentenceCreator {
 		else if (bus.size() > 5) {
 
 			if (index == 1 && (index * 5) <= bus.size()) {
-				if (choice != -1 && choice > 5) {
+				if (choice!=null&&choice != -1 && choice > 5) {
 					rma.setAudioText("对不起，此列表没有您选择的班次信息，请重新选择");
 					return rma;
-				} else if (choice != -1 && choice < 5) {
+				} else if (choice!=null&&choice != -1 && choice < 5) {
+					List <Bus> busDingdan=new ArrayList<Bus>();
+					busDingdan.add(bus.get(choice));
 					String choiceContent = HtmlCenter.getInstance()
-							.getHtmlByList("bus.vm", bus.get(choice), "inputs",
-									"videohtml");
+							.getHtmlByList("bus.vm",busDingdan, "inputs",
+									"demandhtml");
 					responseDisplay.setDataType("1");
 					responseDisplay.setHeight("0.8");
 					responseDisplay.setContent(choiceContent);
@@ -91,14 +104,16 @@ public class BusSentenceCreator implements SentenceCreator {
 
 			} else if (index != 1 && (index * 5) < bus.size()) { // 显示中间结果
 
-				if (choice != -1 && choice > 5) {
+				if (choice!=null&&choice != -1 && choice > 5) {
 					rma.setAudioText("对不起，此列表没有您选择的班次信息，请重新选择");
 					return rma;
 				}
-				if (choice != -1 && choice < 5) {
+				if (choice!=null&&choice != -1 && choice < 5) {
+					List <Bus> busDingdan=new ArrayList<Bus>();
+					busDingdan.add(bus.get((index - 1) * 5 + choice - 1));
 					String choiceContent1 = HtmlCenter.getInstance()
-							.getHtmlByList("bus.vm", bus.get(choice), "inputs",
-									"videohtml");
+							.getHtmlByList("bus.vm", busDingdan, "inputs",
+									"demandhtml");
 					responseDisplay.setDataType("1");
 					responseDisplay.setHeight("0.8");
 					responseDisplay.setContent(choiceContent1);
@@ -119,13 +134,15 @@ public class BusSentenceCreator implements SentenceCreator {
 					rma.setAudioText("对不起，已经没有数据");
 					return rma;
 				}
-				if (choice != -1 && choice > bus.size() % 5) {
+				if (choice!=null&&choice != -1 && choice > bus.size() % 5) {
 					rma.setAudioText("对不起，此列表没有您选择的班次信息，请重新选择");
 					return rma;
-				} else if (choice != -1 && choice <= bus.size() % 5) {
+				} else if (choice!=null&&choice != -1 && choice <= bus.size() % 5) {
+					List <Bus> busDingdan=new ArrayList<Bus>();
+					busDingdan.add(bus.get((index - 1) * 5 + choice - 1));
 					String choiceContent1 = HtmlCenter.getInstance()
 							.getHtmlByList("bus.vm",
-									bus.get((index - 1) * 5 + choice - 1),
+									busDingdan,
 									"inputs", "videohtml");
 					responseDisplay.setDataType("1");
 					responseDisplay.setHeight("0.8");
@@ -143,7 +160,6 @@ public class BusSentenceCreator implements SentenceCreator {
 				rma.setDisplayText(JSON.toJSONString(responseDisplay));
 				return rma;
 			}
-
 		}
 		return rma;
 	}
